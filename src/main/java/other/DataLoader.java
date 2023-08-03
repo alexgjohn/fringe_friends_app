5c977e8e01ac8d4281dfca773701b8a66e60276d
Alex <alexgjohn@outlook.com>
2023-08-03 13:36:12 +0100

    started filling in data loader

--- src/main/java/other/DataLoader.java
+++ src/main/java/other/DataLoader.java
@@ -1,9 +1,29 @@
 package other;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import repositories.FriendRepository;
+import repositories.RecommendationRepository;
+import repositories.ShowRepository;
 
 @Profile("!test")
 @Component
-public class DataLoader {
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    RecommendationRepository recommendationRepository;
+
+    @Autowired
+    FriendRepository friendRepository;
+
+    @Autowired
+    ShowRepository showRepository;
+
+    @Override
+    public void run(ApplicationArguments args){
+
+    }
 }
