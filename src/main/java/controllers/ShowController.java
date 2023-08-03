1a143f091df0d340d2c275fe308cfd3b7144da1b
Alex <alexgjohn@outlook.com>
2023-08-03 15:41:58 +0100

    tried to see single show saved to database, but failed. must look into WHY

--- src/main/java/controllers/ShowController.java
+++ src/main/java/controllers/ShowController.java
@@ -1,4 +1,24 @@
 package controllers;
 
+import models.Show;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repositories.ShowRepository;
+
+import java.util.List;
+
+@RestController
 public class ShowController {
+
+    @Autowired
+    ShowRepository showRepository;
+
+    @GetMapping(value = "/shows")
+    public ResponseEntity<List<Show>> getAllShows(){
+        return new ResponseEntity<>(showRepository.findAll(), HttpStatus.OK);
+    }
+
 }
